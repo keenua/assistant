8f9afbd69aaa7c916618aebdfec5c19e74bfb587
Roman Olshevskyi <roman.olshevskiy@gmail.com>
2023-04-08 06:36:49 +0200

    Added body animations from mixamo (not retargeted yet)

---
+++ Content/Python/convert_mixamo.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:96b433efbc86538da43f9be1aa9b8fd526603fb4bb2f3cf2f8eb6decbb3ead56
+size 1880
---
+++ Content/Python/download_mixamo.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0e160fd1dd27172b9a4f0a3239d559d5d02c5544deeccabf956f518c09699a6
+size 2856
